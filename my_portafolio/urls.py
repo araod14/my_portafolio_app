ce6be49b4d5a6010544594df151b7b180e559880
Daniel Arao <araodaniel14@gmail.com>
2023-04-21 14:58:53 -0400

    blog added

--- my_portafolio/urls.py
+++ my_portafolio/urls.py
@@ -20,7 +20,8 @@ from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('portfolio.urls'))
+    path('', include('portfolio.urls')),
+    path('blog/', include('blog.urls'))
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
