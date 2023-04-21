bbd0adcd564587b14a55a15de983beaf0df9788a
Daniel Arao <araodaniel14@gmail.com>
2023-04-21 13:41:43 -0400

    porfolio ready

--- portfolio/views.py
+++ portfolio/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from .models import Project
 
-# Create your views here.
+
 def index(request):
-    return HttpResponse("holax")
+    projects = Project.objects.all()
+    return render(request, "index.html", {"projects": projects})
