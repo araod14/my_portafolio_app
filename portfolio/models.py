c8cea927c57d2ba4cec30349c8c692b092b7ad5d
Daniel Arao <araodaniel14@gmail.com>
2023-05-04 14:58:23 -0400

    baged added

--- portfolio/models.py
+++ portfolio/models.py
@@ -7,8 +7,8 @@ from datetime import date
 class Project(models.Model):
     title = CharField(max_length=100)
     description = CharField(max_length=250)
-    framework = CharField(max_length=50, null=True)
-    database = CharField(max_length=50, null=True)
+    framework = CharField(max_length=50, blank=True, null=True)
+    database = CharField(max_length=50, blank=True, null=True)
     image = ImageField(upload_to="portfolio/images")
     url = URLField(blank=True)
     date = DateField(default=date.today)
