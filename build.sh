c843072ca0acc993d22749080087a113d7164d29
Daniel Arao <araodaniel14@gmail.com>
2023-05-08 19:21:12 -0400

    .sh modificado permiso de images

--- build.sh
+++ build.sh
@@ -6,6 +6,8 @@ pip install -r requirements.txt
 
 python manage.py collectstatic --no-input
 python manage.py migrate
+chmod 666 /portfolio/images/*
+chmod 666 /blog/images/*
 
 if [[ $CREATE_SUPERUSER ]];
 then
