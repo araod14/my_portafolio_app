795b81d1948e09d3233acb9788454305c69e5799
Daniel Arao <araodaniel14@gmail.com>
2023-05-08 20:15:04 -0400

    staticfiles test

--- my_portafolio/settings.py
+++ my_portafolio/settings.py
@@ -139,6 +139,6 @@ if not DEBUG:    # Tell Django to copy statics to the `staticfiles` directory
     STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
     # Turn on WhiteNoise storage backend that takes care of compressing static files
     # and creating unique names for each version so they can safely be cached forever.
-    STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
