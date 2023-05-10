e7d1e005c0673791a3cc51eff82aef4a41a125a5
Daniel Arao <araodaniel14@gmail.com>
2023-05-09 21:47:24 -0400

    media root change

--- my_portafolio/settings.py
+++ my_portafolio/settings.py
@@ -138,7 +138,7 @@ STATIC_URL = 'static/'
 #MEDIA_ROOT = os.path.join(BASE_DIR, 'media/images/')
 MEDIA_URL = "/media/"
 
-MEDIA_ROOT=os.path.join(BASE_DIR,'/media/')
+MEDIA_ROOT=BASE_DIR,"media"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
